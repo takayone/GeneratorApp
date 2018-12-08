1693267cac4f1fede526835dc89671b00457cd1f
taka yone <tkhtynd1991@gmail.com>
2018-12-08 16:08:44 +0900

    Integrated update actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,7 +7,7 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new
 	end
 
-	 def create
+	def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
@@ -17,5 +17,20 @@ class PortfoliosController < ApplicationController
         format.html { render :new }
       end
     end
-  end
+  	end
+
+  	def edit
+  		@portfolio_item = Portfolio.find(params[:id])
+  	end	
+
+  	def update
+  		@portfolio_item = Portfolio.find(params[:id])
+    	respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+     end
+    end
 end
