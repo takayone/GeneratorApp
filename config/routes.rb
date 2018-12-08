2314ebe8b5f50970aea6887fe9061349aec738cd
taka yone <tkhtynd1991@gmail.com>
2018-12-08 17:55:09 +0900

    configured custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
-  resources :blogs
   resources :portfolios
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  resources :blogs
+	
+  root to: 'pages#home'  
+ 
 end
