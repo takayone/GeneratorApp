55117cebf2805e839229cc7e8d16b2ffe28badd3
taka yone <tkhtynd1991@gmail.com>
2018-12-11 11:08:46 +0900

    completed

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,12 @@ class Blog < ApplicationRecord
 	enum status: { draft: 0, published: 1 }
 	validates_presence_of :title, :body
 	belongs_to :topic
+
+	def self.special_blogs
+		all
+	end	
+
+	def self.featured_blogs
+		limit(2)
+	end	
 end
